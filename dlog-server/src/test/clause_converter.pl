e186e6a1599269446f538bf5ee4f57fd7139a691
kadarb <>
2008-10-04 16:35:11 +0000

    clause converter fixed and completed

--- dlog-server/src/test/clause_converter.pl
+++ dlog-server/src/test/clause_converter.pl
@@ -25,9 +25,17 @@ write_converted_clauses(InFile, OutFile) :-
 
 convert_clauses([], []).
 convert_clauses([List| Ls], [Implication|Is]) :-
-	select(aconcept(Name, Var), List, Body), !,
-	functor(Head, Name, 1),
-	arg(1, Head, Var),
+	(	select(aconcept(Name, Var), List, Body)
+	->	!,
+		functor(Head, Name, 1),
+		arg(1, Head, Var)
+	;	select(not(aconcept(Name, Var)), List, Body)
+	->	!,
+		functor(Head0, Name, 1),
+		arg(1, Head0, Var),
+		Head = \Head0
+	%there are always at least one concept, right?
+	),
 	clean(Body, CBody),
 	Implication = (Head :-- CBody),
 	convert_clauses(Ls, Is).
@@ -40,22 +48,15 @@ clean([Term, Term1|Terms], (CTerm, CTerms)) :-
 	clean([Term1|Terms], CTerms).
 
 %TODO: nconcept?
-clean_term(aconcept(Name, Var), Concept) :-
+clean_term(aconcept(Name, Var), \Concept) :-
 	functor(Concept, Name, 1),
 	arg(1, Concept, Var).
-clean_term(arole(Name, Var1, Var2), Role) :-
+clean_term(arole(Name, Var1, Var2), \Role) :-
 	functor(Role, Name, 2),
 	arg(1, Role, Var1),
 	arg(2, Role, Var2).
-clean_term(not(Term), \CTerm) :-
-	(	Term = aconcept(Name, Var)
-	->	functor(CTerm, Name, 1),
-		arg(1, CTerm, Var)
-	;	Term = arole(Name, Var1, Var2) 
-	->	functor(CTerm, Name, 2),
-		arg(1, CTerm, Var1),
-		arg(2, CTerm, Var2)
-	).
+clean_term(not(Term), CTerm) :-
+	clean_term(Term, \CTerm).
 
 write_implications(Implications, ABox, Stream) :-
 	portray_clauses(Implications, Stream),
@@ -75,8 +76,9 @@ write_abox([Assertion |As], Stream) :-
 		->	functor(Fact, Name, 1),
 			arg(1, Fact, Individual)
 		;	Concept = not(aconcept(Name))
-		->	functor(Fact, Name, 1),
-			arg(1, Fact, not(Individual)) %TODO
+		->	functor(Fact0, Name, 1),
+			arg(1, Fact0, Individual),
+			Fact = \Fact0
 		)
 	;	Assertion = rassertion(arole(Name), Individual1, Individual2)
 	->	functor(Fact, Name, 2),
