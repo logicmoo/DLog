2c3eb0c5c79bec1740fa871d3697f12e44f27b8c
zslevi <>
2009-10-14 12:42:42 +0000

    lambda -> join

--- dlog-server/src/prolog_translator/debug/presql_struct_mem_test_01.pl
+++ dlog-server/src/prolog_translator/debug/presql_struct_mem_test_01.pl
@@ -2,26 +2,26 @@
 :- use_module('../presql_struct2sql.pl').
 :- use_module('../zsl_util.pl').
 
-t(1,lambda(1,[X],mem_abox(2,[X],d,[i1]))).
+t(1,join(1,[X],[mem_abox(2,[X],d,[i1])])).
 
-t(2,lambda(1,[X],mem_abox(2,[X],d,[i2,i3]))).
+t(2,join(1,[X],[mem_abox(2,[X],d,[i2,i3])])).
 
-t(3,lambda(1,[X],union(3,[X],[mem_abox(2,[X],d,[i2,i3]),mem_abox(4,[X],e,[i4])]))).
+t(3,join(1,[X],[union(3,[X],[mem_abox(2,[X],d,[i2,i3]),mem_abox(4,[X],e,[i4])])])).
 
-t(4,lambda(1,[X],(mem_abox(2,[X],a,[1,2,4]),mem_abox(3,[X],b,[2,3])))).
+t(4,join(1,[X],[mem_abox(2,[X],a,[1,2,4]),mem_abox(3,[X],b,[2,3])])).
 
-t(5,lambda(1,[X],
-   (
+t(5,join(1,[X],
+   [
       mem_abox(2,[X],a,[1,2,4]),mem_abox(3,[X,Y],b,[2-5,3-4,4-5]),
       mem_abox(4,[Y],c,[4,5,6])
-   )
+   ]
    )).
    
-t(6,lambda(1,[X],(
+t(6,join(1,[X],[
       mem_abox(2,[X],a,[1,2,4]),mem_abox(3,[X,Y],b,[2-5,3-4,4-5]),
       mem_abox(4,[Y],c,[4,5,6]),
       mem_abox(5,[X,Y],b,[2-5])
-   ))).   
+   ])).   
 
 test(X):-
    t(X,PreSqlStruct),
